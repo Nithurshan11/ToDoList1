35418d632c10f05975125776628f2754078490c1
Nidurshan23 <nidurshan1123@gmail.com>
2026-02-18 11:45:13 +0530

    Added Firebase Analytics to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,22 @@
+// --- Firebase Setup Start ---
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAaXz1HgM1L7MMJQJMvT4YHKtWnrHNSWdw",
+  authDomain: "todolist-7fdb3.firebaseapp.com",
+  projectId: "todolist-7fdb3",
+  storageBucket: "todolist-7fdb3.firebasestorage.app",
+  messagingSenderId: "864899649559",
+  appId: "1:864899649559:web:e2c97f0b553978ff21cc52",
+  measurementId: "G-T31JSNRNZM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// --- Firebase Setup End ---
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -19,4 +38,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
